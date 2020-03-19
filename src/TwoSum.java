6d4648cb9505dd292d7c29154515b0a4ff41c8eb
fubo130 <bo_fu2@baylor.edu>
2020-03-19 01:09:50 -0500

    Q1 Use of hashtable

---
+++ src/TwoSum.java
@@ -0,0 +1,70 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Hash table is a better approach to search element
+ */
+public class TwoSum {
+    public static void main() {
+        int arr[] = {2, 7, 11, 5};
+        int target = 9;
+        BruteForce(arr, target);
+        HashTable(arr, target);
+        Special(arr, target);
+    }
+
+    /**
+     * O (N^2)
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int[] BruteForce(int[] nums, int target) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i+1; j < nums.length; j++) {
+                if (nums[i]+nums[j]==target) {
+                    return new int[] {i, j};
+                }
+            }
+        }
+        throw new IllegalArgumentException("Argument Illegal");
+    }
+
+    /**
+     * Better way of mapping element & index (Look up time O(N) -> O(1))
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int[] HashTable(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int left = target - nums[i];
+            if (map.containsKey(left) && map.get(left) != i) {
+                return new int[] {i, map.get(left)};
+            }
+        }
+        throw new IllegalArgumentException("No solution");
+    }
+
+    /**
+     * Special approach One pass hash table
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int[] Special(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int left = target - nums[i];
+            if (map.containsKey(left)) {
+                return new int[] {i, map.get(left)};
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No solution");
+    }
+}
